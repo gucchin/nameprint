efe0a061a0ea35df388191ccac8352257f542a0e
gucchin <goodtimefriends@gmail.com>
2026-04-05 20:31:51 +0900

    Fix print: Aggressively remove all background/shading artifacts for clean A4 output

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'a4-name-tag-v1';
+const CACHE_NAME = 'a4-name-tag-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -9,6 +9,22 @@ const ASSETS = [
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+/* Automatically delete old caches */
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
